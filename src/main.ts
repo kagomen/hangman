877b8f2747a10251079ce169a03cc893074df72f
kagomen <fu.taz.1102@gmail.com>
2025-07-07 13:42:27 +0900

    add Quiz class

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,29 @@
-console.log("Game Start")
+import rawData from "./data/questions.json"
+
+interface Question {
+  word: string
+  hint: string
+}
+
+class Quiz {
+  constructor(public questions: Question[]) {}
+
+  getNext() {
+    const randomIndex = Math.floor(Math.random() * this.questions.length)
+    const selectedQuestion = this.questions[randomIndex]
+    // 質問リストを選ばれた質問以外で更新する
+    this.questions = this.questions.filter((_, index) => index !== randomIndex)
+    return selectedQuestion
+  }
+
+  hasNext() {
+    return this.questions.length > 0
+  }
+
+  getRemainingQuestionCount() {
+    return this.questions.length
+  }
+}
+
+const questions: Question[] = rawData
+const quiz = new Quiz(questions)
