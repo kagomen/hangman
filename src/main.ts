7443b26988bcb6b888881d06e6ec928b67968af6
kagomen <fu.taz.1102@gmail.com>
2025-07-07 15:55:24 +0900

    add cli-display-feature

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import chalk from "chalk"
+import figlet from "figlet"
+import readline from "readline/promises"
 import rawData from "./data/questions.json"
 
 interface Question {
@@ -27,3 +30,52 @@ class Quiz {
 
 const questions: Question[] = rawData
 const quiz = new Quiz(questions)
+
+// interface UI {
+//   //ユーザの入力を待機
+//   input(): Promise<string>
+//   //ターミナルの画面をクリア
+//   clear(): void
+//   //プログラムがそれ以降の入力を受け付けないようにする
+//   destroy(): void
+//   //色付きメッセージを表示
+//   output(message: string, color?: Color): void
+//   //解答状況を表示
+//   outputAnswer(message: string): void
+// }
+
+type Color = "red" | "green" | "yellow" | "white"
+
+class Cli {
+  private static rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdin,
+  })
+  static async input() {
+    const input = await this.rl.question("文字を入力してください")
+    return input.replaceAll(" ", "").toLowerCase()
+  }
+  static clear() {
+    console.clear()
+  }
+  static destroy() {
+    this.rl.close()
+  }
+  static output(message: string, color: Color = "green") {
+    console.log(chalk[color](message), "\n")
+  }
+  static outputAnswer(message: string) {
+    console.log(figlet.textSync(message, { font: "Star Wars" }), "\n")
+  }
+}
+
+async function run() {
+  Cli.clear()
+  const userInput = await Cli.input()
+  console.log(userInput)
+  Cli.output("hello world")
+  Cli.outputAnswer("generic_")
+  Cli.destroy()
+}
+
+run()
